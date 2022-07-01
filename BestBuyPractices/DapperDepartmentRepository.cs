6eed0485c21c311f9c31f6c51ef2005631655505
Monica Crowe <mcrowe9194@gmail.com>
2022-07-01 00:07:11 -0500

    Finished Project

---
+++ BestBuyPractices/DapperDepartmentRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using Dapper;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BestBuyPractices
+{
+    public class DapperDepartmentRepository : IDepartmentRepository
+    {
+
+        private readonly IDbConnection _connection;
+        public DapperDepartmentRepository(IDbConnection connection)
+        {
+            _connection = connection;
+        }
+
+        public IEnumerable<Department> GetAllDepartments()
+        {
+             var depos = _connection.Query<Department>("SELECT * FROM departments");
+
+            return depos;
+        }
+        
+        public void InsertDepartment(string newDepartmentName)
+        {
+            _connection.Execute("INSERT INTO DEPARTMENTS (Name) VALUES (@departmentName);",
+             new { departmentName = newDepartmentName });
+        }
+    }
+}
--- BestBuyPractices/Program.cs
+++ BestBuyPractices/Program.cs
@@ -12,6 +12,46 @@ namespace BestBuyPractices
         static void Main(string[] args)
         {
 
+            #region Configuration
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            string connString = config.GetConnectionString("DefaultConnection");
+            #endregion
+
+            IDbConnection conn = new MySqlConnection(connString);
+            DapperDepartmentRepository repo = new DapperDepartmentRepository(conn);
+
+            Console.WriteLine("Hello user! Here are the current departments:");
+            Console.WriteLine("Please press enter...");
+            Console.ReadLine();
+
+            var depos = repo.GetAllDepartments();
+            Print(depos);
+            
+            Console.WriteLine("Do you want to add a department?");
+            string userResponse = Console.ReadLine();
+
+            if (userResponse.ToLower() == "yes")
+            {
+                Console.WriteLine("What is the name of your new Department?");
+                userResponse = Console.ReadLine();
+
+                repo.InsertDepartment(userResponse);
+                Print(repo.GetAllDepartments());
+            }
+            Console.WriteLine("Have a great day!");
+        }
+
+        private static void Print(IEnumerable<Department> depos)
+        {
+            foreach(var depo in depos)
+            {
+                Console.WriteLine($"Id: {depo.DepartmentId} Name: {depo.Name} ");
+            }
+
         }
     }
 }
